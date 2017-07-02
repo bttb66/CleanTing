d17e1b1d56422b9e7673a12289e33922ec521a42
DESKTOP-SQTPEVI\xlsdn <dmsal7325@naver.com>
2017-07-02 19:17:56 +0900

    mypage 이용내역 빼고 검사완료

--- test/routes/mypage.js
+++ test/routes/mypage.js
@@ -27,6 +27,7 @@ router.get('/usage/:userId', async (req, res)=>{
 //회원 정보 수정(핸드폰 번호)
 router.put('/phone/:userId', async (req, res)=>{
   try{
+    var connection = await pool.getConnection();
     const userId = req.params.userId;
     const phone = req.headers.phone;
     let query = 'update user set phone=? where userId=?';
@@ -46,8 +47,9 @@ router.put('/phone/:userId', async (req, res)=>{
 //회원 정보 수정(주소)
 router.put('/address/:userId', async (req, res)=>{
   try{
+    var connection = await pool.getConnection();
     const userId = req.params.userId;
-    const address = req.headers.address;
+    const address = req.body.address;
     let query = 'update user set address=? where userId=?';
     await connection.query(query, [address, userId]);
 
@@ -65,6 +67,7 @@ router.put('/address/:userId', async (req, res)=>{
 //회원 정보 수정(비밀번호)
 router.put('/pwd/:userId', async (req, res)=>{
   try{
+    var connection = await pool.getConnection();
     const userId = req.params.userId;
     const pwd = req.headers.pwd;
     let query = 'update user set pwd=? where userId=?';

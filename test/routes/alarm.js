0ceabd7b2a0ebf52bfe31ca973ee3931d5677d51
BTTB66\박세원 <backtothebasic6@gmail.com>
2017-07-03 00:38:29 +0900

    푸시 알람 여부 수정,검사

--- test/routes/alarm.js
+++ test/routes/alarm.js
@@ -24,12 +24,12 @@ router.get('/:userId', async (req, res)=>{
 });
 
 //알람 받기 여부 수정(1, -1)
-router.put('/:push', async(req, res)=>{
+router.put('/:userId', async(req, res)=>{
   try{
     var connection = await pool.getConnection();
-    let query = 'update user set push=-1*push';
-    await connection.query(query);
-    res.status(200).send({message:'push update ok'});
+    let query = 'update user set push=-1*push where userId=? ';
+    await connection.query(query, req.params.userId);
+    res.status(200).send({message:'push alarm update ok'});
   }
   catch (err){
     console.log(err);
